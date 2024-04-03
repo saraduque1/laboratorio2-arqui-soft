0e84818431c3b05c29f5668f8b0c3972dda3e3b8
saraduque1 <sara.duque1@udea.edu.co>
2024-04-03 12:56:05 -0500

    Mi primer commit

---
+++ src/test/java/com/lab2/laboratorio2/Laboratorio2ApplicationTests.java
@@ -0,0 +1,42 @@
+package com.lab2.laboratorio2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+class Laboratorio2ApplicationTests {
+
+	@Autowired
+	DataController dataController;
+
+	@Test
+	void health() {
+		assertEquals("APLICACION FUNCIONANDO OK!", dataController.healthCheck());
+	}
+
+	@Test
+	void version() {
+		assertEquals("La version Actual es: 1.0.0", dataController.version());
+	}
+
+	@Test
+	void nationLength() {
+		Integer nationsLength = dataController.getRandomNations().size();
+		assertEquals(10, nationsLength);
+	}
+
+	@Test
+	void currenciesLength() {
+		Integer currenciesLength = dataController.getRandomnCurrencies().size();
+		assertEquals(20, currenciesLength);
+	}
+
+	@Test
+	void airlinesLength() {
+		Integer airlinesLength = dataController.getRandomAirlines().size();
+		assertEquals(20, airlinesLength);
+	}
+}
